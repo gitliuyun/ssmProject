8b7e57b2c20ceb8efb184d329cc4d20e5f8cc7d7
yang <1217381410@qq.com>
2017-05-02 15:43:42 +0800

    增加shiro控制

--- src/main/java/sy/controller/UserController.java
+++ src/main/java/sy/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import sy.model.User;
 import sy.service.UserService;
 
-@Controller
+/*@Controller
 @RequestMapping("/userController")
 public class UserController {
 
@@ -27,3 +27,16 @@ public class UserController {
 		this.userService = userService;
 	}
 }
+*/
+
+@Controller  
+@RequestMapping("mydemo")  
+public class UserController {  
+    @RequestMapping(value="/getUserInfo")  
+    public String getUserInfo(HttpServletRequest request){  
+        String currentUser = (String)request.getSession().getAttribute("currentUser");  
+        System.out.println("当前登录的用户为[" + currentUser + "]");  
+        request.setAttribute("currUser", currentUser);  
+        return "/user/info";  
+    }  
+} 
